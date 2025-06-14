c1d88ee79fb8500cf5ba6ddbe0f47eb53b4261a4
TenPanelX <85727022+TenPanelX@users.noreply.github.com>
2025-06-14 22:21:17 +0700

    Update FFFFF.lua

--- FFFFF.lua
+++ FFFFF.lua
@@ -1,33 +1,33 @@
-getgenv().Configs = {
-    ["Auto ATM"] = true,
-    ["Black Screen"] = false,
-    ["Rejoin When Bug"] = true,
-    ["Fps Cap"] = 15,
+local LocalPlayer = game.Players.LocalPlayer
 
-    ["Disable Key"] = "G",
-    ["Bank Deposit"] = 200,
-    ["Max Swiper"] = 5,
+local function getNearestATM()
+    local nearestAtm, nearestDistance = nil, math.huge
 
-    ["Hop when die"] = true,
-    ["Hop when die max"] = 2,
-
-    ["Auto Hop"] = {
-        ["Enabled"] = false,
-        ["Cooldown Check"] =  1, -- Minute
-        ["Players Limit"] = 18,
-        ["Car Limit"] = 4,
-    },
-
-    ["Auto Rejoin"] = false,
-    ["Rejoin time [min]"] = 40,
-    ["Car Speed"] = 55
-}
-
-task.spawn(function()
-    succ, err = pcall(function()
-        loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/2b69f230c8d1f815559658d461e5a99f.lua"))()
-    end)
-    if not succ then
-        warn("error: {tostring(p)}")
+    for i, v in next, game:GetService("CollectionService"):GetTagged("ATM") do
+        v.PrimaryPart = v.Area
+        local distance = LocalPlayer:DistanceFromCharacter(v.Part.Position)
+        if distance < nearestDistance then
+            nearestDistance = distance
+            nearestAtm = v
+        end
     end
-end)
+
+    return nearestAtm, nearestDistance
+end
+
+while true do
+    task.wait()
+    local Passed, Statement = pcall(function()
+        local atm = getNearestATM()
+
+        if atm and LocalPlayer:DistanceFromCharacter(atm.Part.Position) <= 30 then
+            local playerPosition = LocalPlayer.Character.PrimaryPart.Position
+            local atmPosition = atm.Part.Position
+
+            local direction = (atmPosition - playerPosition).unit
+            local oppositeDirection = Vector3.new(direction.X, 0, direction.Z).unit
+            local newPosition = Vector3.new(atmPosition.X + oppositeDirection.X * 5, atmPosition.Y, atmPosition.Z + oppositeDirection.Z * 5)
+            atm:SetPrimaryPartCFrame(CFrame.new(newPosition))
+        end
+    end)
+end
